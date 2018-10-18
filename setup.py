045d16ab6667ffaa7fee19352102a58ee32b3585
derlinchen <derlin_nj@163.com>
2018-10-18 10:04:39 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spider_website',
+    version='1.0.0',
+    packages=['spider_website', 'spider_website.model', 'spider_website.spiders'],
+    url='',
+    license='',
+    author='Derlin',
+    author_email='',
+    description=''
+)
---
+++ spider_website/pipelines.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import MySQLdb
+from scrapy.exceptions import DropItem
+import redis
+
+from scrapy import log
+
+import logging
+
+from spider_website.model import proxy, engine, Base, loadSession
+
+Base.metadata.create_all(engine)
+Redis = redis.StrictRedis(host='118.24.106.140', port=6379, db=0, password='derlin2008')
+
+
+# item去重
+class DuplicatesPipeline(object):
+    def process_item(self, item, spider):
+        if Redis.exists('ip_port:%s' % item['ip_port']):
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            Redis.set('ip_port:%s' % item['ip_port'], 1)
+            return item
+
+
+# 数据入库
+class IpProxyPoolPipeline(object):
+    def process_item(self, item, spider):
+        if len(item['ip_port']):
+            a = proxy.Proxy(
+                ip_port=item['ip_port'],
+                type=item['type'],
+                level=item['level'],
+                location=item['location'],
+                speed=item['speed'],
+                lifetime=item['lifetime'],
+                lastcheck=item['lastcheck'],
+                rule_id=item['rule_id'],
+                source=item['source']
+            )
+            session = loadSession()
+            try:
+                session.merge(a)
+                session.commit()
+            except MySQLdb.IntegrityError, e:
+                log.msg("MySQL Error: %s" % str(e), _level=logging.WARNING)
+            return item
+        else:
+            log.msg("ip_port is invalid!", _level=logging.WARNING)
